1c34740f290b5927f9b4fc714fac521978aea777
idreamers <idreamers@dreamtech.tw>
2014-01-15 13:24:22 +0800

    上層修改測試

--- index.php
+++ index.php
@@ -32,7 +32,7 @@
     <div class="container">
 
       <!-- Static navbar -->
-      <div class="navbar navbar-default" role="navigation">
+      <div class="navbar navbar-fixed-top" role="navigation">
         <div class="navbar-header">
           <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
             <span class="sr-only">Toggle navigation</span>
