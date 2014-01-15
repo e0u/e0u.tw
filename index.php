f37023ac18e97e1eee069981f19f55e8b9f40cd0
idreamers <idreamers@dreamtech.tw>
2014-01-15 13:41:30 +0800

    右方下拉選單

--- index.php
+++ index.php
@@ -44,11 +44,11 @@
         <div class="navbar-collapse collapse">
           <ul class="nav navbar-nav">
             <li class="active"><a href="#">首頁</a></li>
-            <li><a href="#about">模擬選課</a></li>
-            <li><a href="#contact">課程資訊</a></li>
+            <li><a href="#vcoruse">模擬選課</a></li>
+            <li><a href="#coruse">課程資訊</a></li>
             <li><a href="#Devlog">開發日誌</a></li>
             <li><a href="#About">關於我們</a></li>
-            <li class="dropdown">
+            <li class="新增選單">
               <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
               <ul class="dropdown-menu">
                 <li><a href="#">Action</a></li>
@@ -65,6 +65,18 @@
             <li><a href="../navbar/">Default</a></li>
             <li><a href="../navbar-static-top/">Static top</a></li>
             <li class="active"><a href="./">Fixed top</a></li>
+            <li class="右邊測試">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
+              <ul class="dropdown-menu">
+                <li><a href="#">Action</a></li>
+                <li><a href="#">Another action</a></li>
+                <li><a href="#">Something else here</a></li>
+                <li class="divider"></li>
+                <li class="dropdown-header">Nav header</li>
+                <li><a href="#">Separated link</a></li>
+                <li><a href="#">One more separated link</a></li>
+              </ul>
+            </li>
           </ul>
         </div><!--/.nav-collapse -->
       </div>
