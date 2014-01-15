397f8e4a60f314876e1af9dc0cd94d9acef0d9bf
idreamers <idreamers@dreamtech.tw>
2014-01-15 13:50:03 +0800

    navbar 修改
    移除部分選項及修改項目

--- index.php
+++ index.php
@@ -47,24 +47,13 @@
             <li><a href="#vcoruse">模擬選課</a></li>
             <li><a href="#coruse">課程資訊</a></li>
             <li><a href="#Devlog">開發日誌</a></li>
-            <li><a href="#About">關於我們</a></li>
-            <li class="dropdown">
-              <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
-              <ul class="dropdown-menu">
-                <li><a href="#">Action</a></li>
-                <li><a href="#">Another action</a></li>
-                <li><a href="#">Something else here</a></li>
-                <li class="divider"></li>
-                <li class="dropdown-header">Nav header</li>
-                <li><a href="#">Separated link</a></li>
-                <li><a href="#">One more separated link</a></li>
-              </ul>
-            </li>
+
+            
           </ul>
           <ul class="nav navbar-nav navbar-right">
-            <li><a href="../navbar/">Default</a></li>
-            <li><a href="../navbar-static-top/">Static top</a></li>
-            <li class="active"><a href="./">Fixed top</a></li>
+            <li><a href="#About">關於我們</a></li>
+            <li><a href="">Link</a></li>
+            <li><a href="">Link</a></li>
             <li class="dropdown">
               <a href="#" class="dropdown-toggle" data-toggle="dropdown">會員資訊 <b class="caret"></b></a>
               <ul class="dropdown-menu">
