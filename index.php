8cc514b8249d359cd9f0e0c3e71d1cb378be0350
idreamers <idreamers@dreamtech.tw>
2014-01-15 17:08:27 +0800

    內文 css 位置修改
    修復原連結無效導致文字重疊

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
     <link href="css/bootstrap.css" rel="stylesheet">
 
     <!-- Custom styles for this template -->
-    <link href="navbar-fixed-top.css" rel="stylesheet">
+    <link href="navbar.css" rel="stylesheet">
 
     <!-- Just for debugging purposes. Don't actually copy this line! -->
     <!--[if lt IE 9]><script src="js/ie8-responsive-file-warning.js"></script><![endif]-->
@@ -62,14 +62,21 @@
               </ul>
             </li>
           </ul>
-        </div><!--/.nav-collapse -->
+        </div>
+        <!--/.nav-collapse -->
       </div>
     </div>
 
     <div class="container">
+      <div class="container">
+        <h1>Hello, world!</h1>
+        <p>This is a template for a simple marketing or informational website. It includes a large callout called a jumbotron and three supporting pieces of content. Use it as a starting point to create something more unique.</p>
+        <p><a class="btn btn-primary btn-lg" role="button">Learn more &raquo;</a></p>
+      </div>
 
+    </div>
 
-    </div> <!-- /container -->
+     <!-- /container -->
 
 
     <!-- Bootstrap core JavaScript
